2c9057cb9727bfab875bb59ec3b80bb21e26de06
Cram-o-dev <132034502+Cram-o-dev@users.noreply.github.com>
2023-05-27 12:32:01 -0700

    Add files via upload

--- data/text/text_6.asm
+++ data/text/text_6.asm
@@ -2,7 +2,9 @@ TeachingHMsText::
 	text "Once a #MON"
 	line "learns an HM, the"
 	cont "technique can't"
-	cont "be replaced."
+	cont "be replaced un-"
+	cont "less you visit"
+	cont "the MOVE DELETER."
 
 	para "Better think care-"
 	line "fully before you"

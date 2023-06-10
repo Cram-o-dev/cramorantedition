c3c528453009b611ec12f33b037ec30bb057c185
Cram-o-dev <132034502+Cram-o-dev@users.noreply.github.com>
2023-06-09 17:27:13 -0700

    Add files via upload

--- scripts/ViridianForestSouthGate.asm
+++ scripts/ViridianForestSouthGate.asm
@@ -1,7 +1,26 @@
 ViridianForestSouthGate_Script:
 	call EnableAutoTextBoxDrawing
+	ld hl, ViridianForestSouthGate_ScriptPointers
+	ld a, [wViridianForestSouthGateCurScript]
+	call CallFunctionInTable
 	ret
+	
+ViridianForestSouthGate_ScriptPointers:
+	dw ViridianForestSouthGateScript0
 
+ViridianForestSouthGateScript0:
+	CheckEventHL EVENT_02D
+	ret z
+	CheckAndSetEventReuseHL EVENT_02C
+	ret nz
+	ld a, HS_OLD_MAN
+	ld [wMissableObjectIndex], a
+	predef HideObject
+	ld a, HS_OLD_MAN_1
+	ld [wMissableObjectIndex], a
+	predef ShowObject
+	ret
+	
 ViridianForestSouthGate_TextPointers:
 	dw ViridianForestEntranceText1
 	dw ViridianForestEntranceText2
